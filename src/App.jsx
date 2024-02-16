076c6dd7535f22859827b23593311e0e348fc416
gaurav909 <gaurav150702@gmail.com>
2024-02-16 23:03:31 +0530

    use UseEffect in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import UseStateHook from './component/hook/UseStateHook'
 import UseReducerHok from './component/useReducer/UseReducerHok'
-
+import UseEffectHook from './component/useeffect/UseEffectHook'
 const App = () => {
   return (
     <div>
@@ -10,6 +10,9 @@ const App = () => {
       <br />
       <h1>UseReducer</h1>
       <UseReducerHok/>
+      <br />
+      <h1>UseEffect</h1>
+      <UseEffectHook/>
     </div>
   )
 }
---
+++ src/component/useeffect/UseEffectHook.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+
+const UseEffectHook = () => {
+    const [count,setCount]=useState(0)
+    
+
+
+
+
+   
+    
+    useEffect(()=>{
+        const fetchData= async()=>{
+            const fetchApi = await fetch('https://jsonplaceholder.typicode.com/todos')
+            const result = await fetchApi.json()
+
+            console.table(result)
+        }
+        
+        fetchData()
+      
+        console.log("UseEffect is Running")
+     
+
+    },[])
+  return (
+    <div>
+        <h1>{count}</h1>
+   
+        <button onClick={()=>setCount(count+1)}>Incerease</button>
+    </div>
+  )
+}
+
+export default UseEffectHook
